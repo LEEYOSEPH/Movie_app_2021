8603a6e99e7a5486cadaeaf93dc3b6d00b53d4a5
LEEYOSEPH <laksmi0628@gmail.com>
2021-02-01 17:47:07 +0900

    배포

--- src/Movies.js
+++ src/Movies.js
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+import "./Movie.css";
 
 function Movie({year, title, summary, poster,genres}){
     return (
@@ -9,7 +10,7 @@ function Movie({year, title, summary, poster,genres}){
             <h3 className="movie_title">{title}</h3>
             <h5 className="movie_year">{year}</h5>
            <ul className="movie__genres">{genres.map((genre,index) => (<li ley={index}className="genres_genre">{genre}</li>))}</ul>
-            <p className="movie_summary">{summary}</p>
+            <p className="movie_summary">{summary.slice(0,140)}...</p>
         </div>
     </div>
     )
